c44efb2a646a76b9e21181fcc04e0db71d8ad0f2
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-04-25 16:30:15 +0000

    shell 0.3

--- _getline.c
+++ _getline.c
@@ -14,47 +14,46 @@ int position = 0;
 char *buffer = malloc(sizeof(char) * bufsize);
 int c;
 
-if (!buffer) 
+if (!buffer)
 {
 fprintf(stderr, "./hsh: allocation error\n");
 exit(EXIT_FAILURE);
 }
 
-while (1) 
+while (1)
 {
-  /* Read a character */
+/* Read a character */
 c = getchar();
 
-  /* check if Ctrl-D in entered */  
+/* check if Ctrl-D in entered */
 if (c == -1)
 exit(EXIT_SUCCESS);
-    
-  /* If we hit EOF or "\n", replace it */
-  /* with the null terminator and return. */
+
+/* If we hit EOF or "\n", replace it */
+/* with the null terminator and return. */
 if (c == EOF || c == '\n') {
 buffer[position] = '\0';
 return buffer;
 }
-  
-else 
+
+else
 {
 buffer[position] = c;
 }
 position++;
 
-    
-  /* If we have exceeded the buffer, reallocate. */
+/* If we have exceeded the buffer, reallocate. */
 if (position >= bufsize)
 {
 bufsize += bufsize;
 buffer = realloc(buffer, bufsize);
-if (!buffer) 
+if (!buffer)
 {
 fprintf(stderr, "./hsh: allocation error\n");
 exit(EXIT_FAILURE);
 }
 }
 }
-  
+
 return (buffer);
 }
